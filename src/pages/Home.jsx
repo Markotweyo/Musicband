a0c7b156cf48e7a486a12a67368fa4267839854d
Mark Otweyo <markotweyo99@gmail.com>
2021-10-29 07:28:31 +0300

    Initial react set up

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+import Footer from '../components/Footer'
+import Category from '../components/Category'
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Slider/>
+            <Category/>
+            <Footer/>
+        </div>
+    )
+}
+export default Home;
